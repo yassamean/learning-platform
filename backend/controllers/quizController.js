5ca09145a4b013db7b2770235ecc4a553805a420
Yassamin Asghari <yassamin_asgari@yahoo.de>
2024-07-15 11:29:01 +0200

    First pass at quiz endpoints

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -2,6 +2,7 @@ import {
   Course,
   Lecture,
   QuizQuestion,
+  QuizStudentAnswer,
   User,
   UserData,
 } from "../models/index.js";
@@ -145,6 +146,61 @@ export async function deleteQuestion(req, res) {
   }
 }
 
+export async function submitStudentAnswer(req, res) {
+  try {
+    const questionId = req.params.questionId;
+    const studentId = req.query.userId;
+    const studentAnswer = req.body.answer;
+
+    const question = await QuizQuestion.findById(questionId);
+    if (!question) {
+      throw new Error(`Question with ID ${questionId} not found`);
+    }
+
+    const correctAnswer = question.possibleAnswers.find(
+      (answer) => answer.isCorrect
+    );
+
+    const answeredCorrectly = correctAnswer.answerText === studentAnswer;
+
+    await QuizStudentAnswer.create({
+      lectureId: question.lectureId,
+      questionId,
+      studentId,
+      answeredCorrectly,
+    });
+
+    res.status(200).json({ answeredCorrectly });
+  } catch (error) {
+    console.error("Error submitting answer for student", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function nextQuizQuestion(req, res) {
+  try {
+    const lectureId = req.params.lectureId;
+    const studentId = req.query.userId;
+
+    const nextQuestion = await getNextQuizQuestion({ lectureId, studentId });
+
+    // We don't want to send the correct answer info along
+    // in case the student knows how to read network calls :)
+    res.status(200).json({
+      ...nextQuestion,
+      possibleAnswers: nextQuestion.possibleAnswers.map(
+        (answer) => answer.answerText
+      ),
+    });
+  } catch (error) {
+    console.error(
+      `Error getting next question for lecture ${lectureId} for student ${studentId}`,
+      error
+    );
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
 // --------------------
 // ----- HELPERS ------
 // --------------------
@@ -176,4 +232,105 @@ async function getQuizListForCourseData({ user, course }) {
       numQuestions: lecture.questionCount,
     })),
   };
+}
+
+async function getNextQuizQuestion({ lectureId, studentId }) {
+  const allQuestions = await QuizQuestion.find({ lectureId });
+  const allStudentAnswers = await QuizStudentAnswer.find({
+    lectureId,
+    studentId,
+  });
+
+  // { questionId: [Timestamp || null] }
+  // If there is a timestamp, the question has been answered correctly before and
+  // the time represents the most recent correct answer.
+  // If there is no timestamp, it means the question has been answered before
+  // but it was answered incorrectly.
+  const mostRecentCorrectAnswers = allStudentAnswers.reduce(
+    (result, studentAnswer) => {
+      const questionId = studentAnswer.questionId.toString();
+      const previousCorrectAnswerTime = result[questionId];
+
+      if (studentAnswer.answeredCorrectly && previousCorrectAnswerTime) {
+        result[questionId] =
+          studentAnswer.createdAt > previousCorrectAnswerTime
+            ? studentAnswer.createdAt
+            : previousCorrectAnswerTime;
+      } else if (answeredCorrectly) {
+        result[questionId] = studentAnswer.createdAt;
+      } else result[questionId] = null;
+    },
+    {}
+  );
+
+  // --------------------------------------------
+  // Our basic "Spaced Repetition" algorithm
+  // --------------------------------------------
+
+  // 1. Get all questions that the student has NOT answered.
+  // If there are any, return the earliest-created question.
+  const unansweredQuestions = allQuestions
+    .filter((question) => {
+      const answeredQuestionIds = Object.keys(mostRecentCorrectAnswers);
+      return !answeredQuestionIds.includes(question._id.toString());
+    })
+    .toSorted(sortQuestionByCreatedAt);
+
+  console.log(`${unansweredQuestions.length} unanswered questions:`);
+  console.log(unansweredQuestions);
+
+  if (unansweredQuestions.length > 0) {
+    return unansweredQuestions[0];
+  }
+
+  // 2. If all questions have been answered,
+  // return a question that the student has answered but never gotten right.
+  const questionNeverAnsweredCorrectly = Object.entries(
+    mostRecentCorrectAnswers
+  ).find((_, timestamp) => timestamp === null);
+  console.log("Question never answered correctly:");
+  console.log(questionNeverAnsweredCorrectly);
+  if (questionNeverAnsweredCorrectly) {
+    return allQuestions.find(
+      (question) => question._id.toString() === questionNeverAnsweredCorrectly
+    );
+  }
+
+  // 3. If user has answered all questions right,
+  // return the question that was answered correctly the longest time ago.
+  const questionsAnsweredCorrectly = Object.entries(mostRecentCorrectAnswers)
+    .filter(([_, timestamp]) => timestamp)
+    .toSorted(sortByTimestamp);
+
+  console.log("Question answered correctly:");
+  console.log(questionsAnsweredCorrectly);
+  if (questionsAnsweredCorrectly) {
+    return allQuestions.find(
+      (question) => question._id.toString() === questionsAnsweredCorrectly[0][0]
+    );
+  }
+
+  // 4. If we still haven't found anything, something has gone terribly wrong
+  // but we'll just return the first question that exists
+  return allQuestions[0];
+}
+
+function sortQuestionByCreatedAt(questionA, questionB) {
+  if (questionA.createdAt < questionB.createdAt) {
+    return -1;
+  } else if (questionB.createdAt < questionA.createdAt) {
+    return 1;
+  }
+
+  return 0;
+}
+
+function sortByTimestamp([_, timestampA], [_, timestampB]) {
+  if (timestampA < timestampB) {
+    return -1;
+  } else if (timestampB < timestampA) {
+    return 1;
+  }
+
+  return 0;
 }
--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -7,6 +7,8 @@ import {
   getQuizList,
   getQuizListForCourse,
   getTeacherQuizOverview,
+  submitStudentAnswer,
+  nextQuizQuestion,
 } from "../controllers/quizController.js";
 
 const router = Router();
@@ -15,17 +17,19 @@ router.get("/quizzes", getQuizList);
 router.get("/courses/:courseId/quizzes", getQuizListForCourse);
 
 // For students
-// router.post("/courses/:courseId/lectures/:lectureId/quiz/answer", addAnswer);
-
+router.post("/questions/:questionId/answer", submitStudentAnswer);
+router.get(
+  "/courses/:courseId/lectures/:lectureId/nextQuestion",
+  nextQuizQuestion
+);
 // For teachers
-router.get("/questions/:questionId", getQuestion);
-router.patch("/questions/:questionId", updateQuestion);
-router.post("/courses/:courseId/lectures/:lectureId/questions", addQuestion);
-router.post("/questions/:questionId/delete", deleteQuestion);
-
 router.get(
   "/courses/:courseId/lectures/:lectureId/manageQuiz",
   getTeacherQuizOverview
 );
+router.post("/courses/:courseId/lectures/:lectureId/questions", addQuestion);
+router.get("/questions/:questionId", getQuestion);
+router.patch("/questions/:questionId", updateQuestion);
+router.post("/questions/:questionId/delete", deleteQuestion);
 
 export default router;
