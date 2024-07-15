68a387f49abf73b20d22c621c2daa27d0c59e5a1
Yassamin Asghari <yassamin_asgari@yahoo.de>
2024-07-15 16:08:30 +0200

    Loading for courses page

--- frontend/src/pages/CourseListPage.jsx
+++ frontend/src/pages/CourseListPage.jsx
@@ -8,7 +8,7 @@ import { API_BASE_URL } from "../config";
 import { useAuth } from "../context/UserContext";
 
 export default function CourseListPage({ search }) {
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState(null);
   const { user } = useAuth();
   const [enrollments, setEnrollments] = useState([]);
 
@@ -66,74 +66,89 @@ export default function CourseListPage({ search }) {
           )}
         </div>
       </div>
-      <div className="pt-6 dark:text-slate-200">
-        {courses.length === 0 && <p>{noCoursesMessage}</p>}
-      </div>
-      <ul role="list" className="flex flex-col grow gap-y-2 pt-5">
-        {courses
-          .filter((course) => (search ? course.name.includes(search) : course))
-          .map((course) => (
-            <Link
-              to={`/courses/${course._id}`}
-              key={course._id}
-              className="flex cursor-pointer group rounded-lg p-5 bg-gray-50 hover:bg-gray-200 justify-between dark:hover:bg-sky-900 dark:bg-slate-800"
-            >
-              <div className="flex min-w-0 gap-x-4">
-                <img
-                  className="h-12 w-12 flex-none rounded-full"
-                  src={"/course-pictures/" + course.image}
-                  alt=""
-                />
-                <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900 dark:text-slate-200">
-                    {course.name}
-                  </p>
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500 dark:text-slate-400">
-                    {course.semester}
-                  </p>
-                </div>
-              </div>
-              <div className="flex gap-x-6">
-                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                  <p className="text-sm leading-6 text-gray-900 dark:text-slate-400">
-                    Updated {new Date(course.updatedAt).toLocaleString()}
-                  </p>
-                  {enrollments?.includes(course._id) ? (
-                    <div className="mt-1 flex items-center gap-x-1.5">
-                      <div className="flex-none rounded-full bg-sky-500/20 p-1">
-                        <div className="h-1.5 w-1.5 rounded-full bg-sky-500" />
+      {!courses && (
+        <div className="pt-6 dark:text-slate-200">
+          <p>Loading courses...</p>
+        </div>
+      )}
+      {courses && (
+        <>
+          {courses.length === 0 && (
+            <div className="pt-6 dark:text-slate-200">
+              <p>{noCoursesMessage}</p>
+            </div>
+          )}
+          {courses.length > 0 && (
+            <ul role="list" className="flex flex-col grow gap-y-2 pt-5">
+              {courses
+                .filter((course) =>
+                  search ? course.name.includes(search) : course
+                )
+                .map((course) => (
+                  <Link
+                    to={`/courses/${course._id}`}
+                    key={course._id}
+                    className="flex cursor-pointer group rounded-lg p-5 bg-gray-50 hover:bg-gray-200 justify-between dark:hover:bg-sky-900 dark:bg-slate-800"
+                  >
+                    <div className="flex min-w-0 gap-x-4">
+                      <img
+                        className="h-12 w-12 flex-none rounded-full"
+                        src={"/course-pictures/" + course.image}
+                        alt=""
+                      />
+                      <div className="min-w-0 flex-auto">
+                        <p className="text-sm font-semibold leading-6 text-gray-900 dark:text-slate-200">
+                          {course.name}
+                        </p>
+                        <p className="mt-1 truncate text-xs leading-5 text-gray-500 dark:text-slate-400">
+                          {course.semester}
+                        </p>
                       </div>
-                      <p className="text-xs leading-5 text-gray-500 dark:text-slate-400">
-                        Enrolled
-                      </p>
                     </div>
-                  ) : course.isOpenToEnroll ? (
-                    <div className="mt-1 flex items-center gap-x-1.5">
-                      <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                        <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                    <div className="flex gap-x-6">
+                      <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                        <p className="text-sm leading-6 text-gray-900 dark:text-slate-400">
+                          Updated {new Date(course.updatedAt).toLocaleString()}
+                        </p>
+                        {enrollments?.includes(course._id) ? (
+                          <div className="mt-1 flex items-center gap-x-1.5">
+                            <div className="flex-none rounded-full bg-sky-500/20 p-1">
+                              <div className="h-1.5 w-1.5 rounded-full bg-sky-500" />
+                            </div>
+                            <p className="text-xs leading-5 text-gray-500 dark:text-slate-400">
+                              Enrolled
+                            </p>
+                          </div>
+                        ) : course.isOpenToEnroll ? (
+                          <div className="mt-1 flex items-center gap-x-1.5">
+                            <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                              <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                            </div>
+                            <p className="text-xs leading-5 text-gray-500 dark:text-slate-400">
+                              Open
+                            </p>
+                          </div>
+                        ) : (
+                          <div className="mt-1 flex items-center gap-x-1.5">
+                            <div className="flex-none rounded-full bg-red-500/20 p-1">
+                              <div className="h-1.5 w-1.5 rounded-full bg-red-500" />
+                            </div>
+                            <p className="text-xs leading-5 text-gray-500 dark:text-slate-400">
+                              Closed
+                            </p>
+                          </div>
+                        )}
                       </div>
-                      <p className="text-xs leading-5 text-gray-500 dark:text-slate-400">
-                        Open
-                      </p>
-                    </div>
-                  ) : (
-                    <div className="mt-1 flex items-center gap-x-1.5">
-                      <div className="flex-none rounded-full bg-red-500/20 p-1">
-                        <div className="h-1.5 w-1.5 rounded-full bg-red-500" />
+                      <div className="flex items-center">
+                        <ChevronDoubleRightIcon className="h-6 w-6 dark:text-slate-200 dark:group-hover:text-sky-400"></ChevronDoubleRightIcon>
                       </div>
-                      <p className="text-xs leading-5 text-gray-500 dark:text-slate-400">
-                        Closed
-                      </p>
                     </div>
-                  )}
-                </div>
-                <div className="flex items-center">
-                  <ChevronDoubleRightIcon className="h-6 w-6 dark:text-slate-200 dark:group-hover:text-sky-400"></ChevronDoubleRightIcon>
-                </div>
-              </div>
-            </Link>
-          ))}
-      </ul>
+                  </Link>
+                ))}
+            </ul>
+          )}
+        </>
+      )}
     </div>
   );
 }
