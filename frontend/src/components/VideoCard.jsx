aa66e899270671b45a8c682d3dc880edb35534ad
Sebastian Gleixner <basti7371@gmail.com>
2024-07-17 13:19:17 +0200

    Added Video Thumbnails

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,13 +1,37 @@
 import { PlayCircleIcon } from "@heroicons/react/16/solid";
 import { Link } from "react-router-dom";
 import { formatDistanceToNow } from "date-fns";
+import ReactPlayer from "react-player";
 
 export default function VideoCard(params) {
   return (
     <Link to={params.path}>
       <div className="group p-1 rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-300 dark:bg-slate-800 dark:hover:bg-sky-900">
-        <div className="flex border rounded-lg aspect-video bg-gray-900 items-center justify-center">
-          <PlayCircleIcon className="text-white w-20 h-20"></PlayCircleIcon>
+        <div className="flex border p-1 rounded-lg aspect-video bg-gray-900 items-center justify-center">
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            light={
+              params.data.videoUrl.includes("youtu") ? (
+                true
+              ) : (
+                <ReactPlayer
+                  width="100%"
+                  height="100%"
+                  url={
+                    params.data.videoUrl
+                      ? params.data.videoUrl
+                      : "/uploads/videos/example.mp4"
+                  }
+                ></ReactPlayer>
+              )
+            }
+            url={
+              params.data.videoUrl
+                ? params.data.videoUrl
+                : "/uploads/videos/example.mp4"
+            }
+          ></ReactPlayer>
         </div>
         <div className="line-clamp-2 font-bold dark:text-slate-200 group-hover:dark:text-sky-400">
           {params.data.title}
